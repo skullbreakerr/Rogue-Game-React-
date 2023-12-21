6151fdd3c67d8af32189b8713ecac5ce12eea74b
skullbreakerr <96946235+skullbreakerr@users.noreply.github.com>
2023-12-21 12:46:05 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./style.css";
 import ReactRogue from './ReactRogue.js';
 const App=()=> (
   <div className="App">
+    <h1 id="title">Rogue Game</h1>
+    <h4 id="warning">Work in Progress</h4>
    <ReactRogue width={40} height={40} tilesize={16}/>
   </div>
 );
